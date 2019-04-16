77e4199f6fa7d153a22bd03fb9e79c01c6d3fe53
Pogayo <pogayo17@alustudent.com>
2019-04-16 19:42:20 +0200

    Wrote the sign-up page

---
+++ Shop/PHP/footer.php
@@ -0,0 +1,14 @@
+<div class="footer">
+    <!--<span class="white-text" style="justify-self: flex-start;margin:20px">&copy Radiant Ochre 2019</span>-->
+        <i style="font-style: normal">
+            <a class="white-text no-underline-link padding-left-15" href="#">Home</a>
+            <a class="white-text no-underline-link padding-left-15" href="#">About</a>
+            <a class="white-text no-underline-link padding-left-15" href="#">Contact</a>
+
+        </i>
+     </div>
+
+
+
+
+
---
+++ Shop/PHP/signup-front.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Radiant-contact</title>
+
+        <link href="../CSS/shop.css" type="text/css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+    <body>
+    <div class="width-full" style="height:350px">
+        <img src="../images/suite-bedroom.jpg" alt="bed" class="full-width-height-image"/>
+    </div>
+    <h1 class="center-text">Welcome</h1><br>
+    <div  class="width-full"><!--Contains the sign-up form-->
+    <form action="signup.php" method="post" class="width-full flex flex-column flex-column-center-all">
+        <input type="text" name="uname" placeholder="User Name" class="log-sign-input"><br/>
+
+        <input type="email" name="email" placeholder="Email" class="log-sign-input"><br/>
+
+        <select class="log-sign-input">
+            <option>You are a Seller</option>
+            <option>You are a Buyer</option>
+
+        </select><br/>
+
+
+        <input type="Password" name="pass" placeholder="Password" class="log-sign-input"><br/><br>
+
+
+        <button  type="submit" class="black-button-orange-text log-sign-input"><b>Login</b></button><br>
+    </form>
+    </div>
+    <div class="width-full"><!--contains already have account to redirect-->
+        <p class="center-text">Already have an account? <a href="login-front.php">Log in here</a></p>
+        <?php include 'footer.php';?>
+    </div>
+    </body>
+
+</html>
